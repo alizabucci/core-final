f1579beb08c788a5343f7c5655b5a86bba1165f5
alizabucci <69876564+alizabucci@users.noreply.github.com>
2020-11-14 22:18:05 -0500

    core three final retry

---
+++ functions.js
@@ -0,0 +1,19 @@
+/**
+ * Example of starting a plugin with options.
+ * I am just passing some of the options in the following example.
+ * you can also start the plugin using $('.marquee').marquee(); with defaults
+*/
+$('.marquee').marquee({
+	//duration in milliseconds of the marquee
+	duration: 40000,
+	//gap in pixels between the tickers
+	gap: 0,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover:false,
+  startVisible: true,
+});
